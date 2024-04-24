790b32b00bf5562387af0538495f0899e4f658f5
ameyaZope <zopeameya@gmail.com>
2024-04-24 18:07:00 -0400

    Adding backend APIs for choropleth map

--- backend/server.py
+++ backend/server.py
@@ -14,6 +14,11 @@ app = Flask(__name__)
 app.config.from_pyfile("settings.py")
 
 data = pd.read_csv('../data/final_data.csv')
+file_ptr_world_polygons = open('./atlas/world_polygons_simplified.json')
+world_polygons_topo_json = json.load(file_ptr_world_polygons)
+
+file_ptr_lines_polygons = open('./atlas/world_lines_simplified.json')
+lines_polygons_topo_json = json.load(file_ptr_lines_polygons)
 
 @app.route("/apis/data/scatterplot", methods=['GET'])
 def get_mds_data_plot():
@@ -22,14 +27,26 @@ def get_mds_data_plot():
         'data': loads(data.to_json(orient="records"))
     } 
 
-@app.route("/apis/data/choromap",methods=['GET'])
+
+@app.route("/apis/data/choroplethmap", methods=['GET'])
 def get_choro_plot():
     return {
         # TODO : Remove the first 10 data instances, this is just for testing
         'data': loads(data.to_json(orient="records"))
     } 
-    
+
+
+@app.route("/apis/data/geospatial/world/lines", methods=['GET'])
+def get_world_polygons_map_data():
+    return lines_polygons_topo_json
+
+
+@app.route("/apis/data/geospatial/world/polygons", methods=['GET'])
+def get_world_lines_map_data():
+    return world_polygons_topo_json
+
 
 if __name__ == '__main__':
     # Make the server publicly available
     app.run(host='0.0.0.0', port=8080, debug=True)
+    file_ptr_world_polygons.close()
