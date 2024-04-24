03c17922bdcb82838e0f75512101030f2f8ea37e
Sakshi Sinha <shrishtisakshi311@gmail.com>
2024-04-24 13:44:08 -0400

    Server changes (#1)
    * Server changes
    
    * Simplifying changes
    
    * Fixes
    
    * adding final changes
    
    * Push initial changes

--- backend/server.py
+++ backend/server.py
@@ -2,6 +2,7 @@ from json import loads
 from flask import Flask
 import numpy as np
 import pandas as pd
+import json
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
@@ -14,14 +15,20 @@ app.config.from_pyfile("settings.py")
 
 data = pd.read_csv('../data/final_data.csv')
 
-
 @app.route("/apis/data/scatterplot", methods=['GET'])
 def get_mds_data_plot():
     return {
         # TODO : Remove the first 10 data instances, this is just for testing
         'data': loads(data.to_json(orient="records"))
-    }
+    } 
 
+@app.route("/apis/data/choromap",methods=['GET'])
+def get_choro_plot():
+    return {
+        # TODO : Remove the first 10 data instances, this is just for testing
+        'data': loads(data.to_json(orient="records"))
+    } 
+    
 
 if __name__ == '__main__':
     # Make the server publicly available
