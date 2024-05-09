10e81acd1bbbb44f6fc970b5f6a023242ed60e80
ameyaZope <zopeameya@gmail.com>
2024-05-09 01:21:20 -0400

    Adding better font placing for Histogram

--- frontend/src/components/histogram.js
+++ frontend/src/components/histogram.js
@@ -12,9 +12,9 @@ function Histogram({ currColName, numBins = 20, currColDispName, year }) {
 
 
 	useEffect(() => {
-		var margin = { top: 30, right: 20, bottom: 50, left: 50 },
+		var margin = { top: 50, right: 20, bottom: 80, left: 50 },
 			width = 300 - margin.left - margin.right,
-			height = 300 - margin.top - margin.bottom;
+			height = 330 - margin.top - margin.bottom;
 
 		var svgSelected = d3.select("#histogram");
 		svgSelected.selectAll("*").remove();
@@ -56,7 +56,7 @@ function Histogram({ currColName, numBins = 20, currColDispName, year }) {
 				.style("text-anchor", "end")
 				.style("font", "bold 16px Comic Sans MS");
 			svgRef.current.append("text")
-				.attr("transform", `translate(${width / 2}, ${height + margin.bottom})`)
+				.attr("transform", `translate(${width / 2}, ${height + margin.bottom/2 + 5})`)
 				.style("text-anchor", "middle")
 				.style("font", "bold 16px Comic Sans MS")
 				.text(`${currColDispName}`);
@@ -146,9 +146,9 @@ function Histogram({ currColName, numBins = 20, currColDispName, year }) {
 		}
 		svgRef.current.selectAll('rect').remove();
 
-		var margin = { top: 30, right: 20, bottom: 50, left: 50 },
+		var margin = { top: 50, right: 20, bottom: 80, left: 50 },
 			width = 300 - margin.left - margin.right,
-			height = 300 - margin.top - margin.bottom;
+			height = 330 - margin.top - margin.bottom;
 
 		d3.json(`/apis/data/histogram/${year}`).then(function (histogramData) {
 			histogramData = histogramData['data']
